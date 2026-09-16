33b66f37bb95caa703a50d139bbd7678e077690a
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-16 08:25:31 +0300

    feat: add web permissions management foundation

---
+++ app/Http/Controllers/PermissionWebController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+use Spatie\Permission\Models\Permission;
+
+class PermissionWebController extends Controller
+{
+    public function index(): View
+    {
+        $permissions = Permission::withCount('roles')
+            ->orderBy('name')
+            ->get();
+
+        return view('permissions.index', [
+            'permissions' => $permissions,
+            'title' => __('Permissions'),
+        ]);
+    }
+}
