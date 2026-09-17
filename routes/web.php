db254e3d31e12f84f0f47550daf1a3e4c4d0ca6e
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-17 11:53:55 +0300

    feat: expose operational map data route

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,12 @@ Route::middleware(['auth', 'active', 'permission:tasks.view'])->get('/work-order
 Route::middleware(['auth', 'active', 'permission:tasks.update|tasks.assign|tasks.transition'])->put('/work-orders/{workOrder}', [WorkOrderWebController::class, 'update'])->name('work-orders.update');
 Route::middleware(['auth', 'active', 'permission:tasks.delete'])->delete('/work-orders/{workOrder}', [WorkOrderWebController::class, 'destroy'])->name('work-orders.destroy');
 
-Route::middleware(['auth', 'active', 'permission:gis.view|datasets.view'])->group(function () { Route::get('/gis', [DashboardController::class, 'index'])->name('gis.index'); Route::get('/map', [GisController::class, 'index'])->name('map.index'); });
+Route::middleware(['auth', 'active', 'permission:gis.view|complaints.view|tasks.view|datasets.view'])->group(function () {
+    Route::get('/gis', [DashboardController::class, 'index'])->name('gis.index');
+    Route::get('/map', [GisController::class, 'index'])->name('map.index');
+    Route::get('/map/data', [GisController::class, 'operationalData'])->name('map.data');
+});
+
 Route::middleware(['auth', 'active', 'permission:datasets.view'])->get('/datasets', [DatasetWebController::class, 'index'])->name('datasets.index');
 Route::middleware(['auth', 'active', 'permission:datasets.create'])->group(function () { Route::get('/datasets/create', [DatasetWebController::class, 'create'])->name('datasets.create'); Route::post('/datasets', [DatasetWebController::class, 'store'])->name('datasets.store'); });
 Route::middleware(['auth', 'active', 'permission:datasets.view'])->group(function () { Route::get('/datasets/{dataset}/fields', [DatasetFieldWebController::class, 'index'])->name('datasets.fields.index'); Route::get('/datasets/{dataset}/records', [DatasetRecordWebController::class, 'index'])->name('datasets.records.index'); Route::get('/datasets/{dataset}', [DatasetWebController::class, 'show'])->name('datasets.show'); });
